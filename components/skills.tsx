802dcf1f1b38c83630cef05a017a26a782595f30
kadoshita <sublimer.me@gmail.com>
2022-09-03 21:14:59 +0900

    Update skill level

--- components/skills.tsx
+++ components/skills.tsx
@@ -18,28 +18,28 @@ const skills = [
     { skill: 'Network', level: 7 },
     { skill: 'IoT', level: 5 },
     { skill: 'Operation', level: 7 },
-    { skill: 'Monitoring', level: 5 },
-    { skill: 'Cloud', level: 6 },
+    { skill: 'Monitoring', level: 7 },
+    { skill: 'Cloud', level: 7 },
     { skill: 'Remote Work', level: 8 }
 ].sort((a, b) => a.skill.localeCompare(b.skill));
 
 const languageSkills = [
     { lang: 'JavaScript', level: 10 },
-    { lang: 'TypeScript', level: 7 },
-    { lang: 'C#', level: 5 },
-    { lang: 'Java', level: 4 },
+    { lang: 'TypeScript', level: 10 },
+    { lang: 'C#', level: 7 },
+    { lang: 'Java', level: 3 },
     { lang: 'Go', level: 3 },
-    { lang: 'C/C++', level: 4 },
-    { lang: 'ShellScript', level: 3 }
+    { lang: 'C/C++', level: 5 },
+    { lang: 'ShellScript', level: 5 }
 ].sort((a, b) => a.lang.localeCompare(b.lang));
 
 const techSkills = [
     { name: 'WebRTC', level: 10 },
-    { name: 'Docker', level: 7 },
+    { name: 'Docker', level: 6 },
     { name: 'Windows Server', level: 7 },
     { name: 'NEC IX', level: 8 },
-    { name: 'Kubernetes', level: 3 },
-    { name: 'Prometheus', level: 6 },
+    { name: 'Kubernetes', level: 6 },
+    { name: 'Prometheus', level: 5 },
     { name: 'Ubuntu', level: 6 },
     { name: 'Android', level: 4 }
 ].sort((a, b) => a.name.localeCompare(b.name));
